9fe7df2672b51862ffe19d09bfcdffa6983b4014
Milan Pecov <milan@showpass.com>
2022-11-20 22:27:47 -0700

    github workflow - use postgresql

--- .github/conf/settings.py
+++ .github/conf/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db.postgresql',
     }
 }
 
